54596268e69e27c9200b35fd32c72f1aa78d27e0
Pratik8999 <joshipratik8999@gmail.com>
2023-03-05 19:04:40 +0530

    Setting Modified.

--- user_registration_api/user_registration_api/settings.py
+++ user_registration_api/user_registration_api/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = 'static/'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = "priks9998@gmail.com"
-EMAIL_HOST_PASSWORD = "*********"
+EMAIL_HOST_PASSWORD = "rclpkpbsnvvqxlss"
 EMAIL_USE_TLS = True
 
 
